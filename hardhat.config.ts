256698b61819aa66ee3fd13577d598e65ab12e6d
Kazuma Shimomiya <shimomiya_@indent.co.jp>
2022-11-21 10:29:10 +0900

    Added deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const { PRIVATE_KEY, ETHERSCAN_API, POLYGON_URL } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: POLYGON_URL,
+      accounts: [PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API,
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [owner] = await ethers.getSigners();
+  console.log("Owner is:", owner.address);
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const contractFactory = await ethers.getContractFactory("Social");
+  const contract = await contractFactory.deploy();
+  await contract.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log("Contract deployed to:", contract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
